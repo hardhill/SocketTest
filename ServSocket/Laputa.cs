8c298a0c6714aacdb8230eb87c8892d0e82f449f
W.Bielecki <hardhill@mail.ru>
2020-07-22 12:57:43 +0800

    Initial commit

---
+++ ServSocket/Laputa.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace ServSocket
+{
+    public class Laputa : WebSocketBehavior
+    {
+        protected override Task OnMessage(MessageEventArgs e)
+        {
+            string msg = "Hello";
+
+            Send(msg);
+            return OnMessage(e);
+        }
+    }
+}
---
+++ ServSocket/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+
+namespace ServSocket
+{
+    class Program
+    {
+        static IPAddress serverIp = IPAddress.Any;
+        static int serverPort = 0;
+        static bool ssl = false;
+
+        static void Main(string[] args)
+        {
+            var wssv = new WebSocketServer(serverIp,9000);
+            wssv.AddWebSocketService<Laputa>("/Laputa");
+            wssv.Start();
+            Console.ReadKey(true);
+            wssv.Stop();
+
+        }
+
+
+    }
+
+}
